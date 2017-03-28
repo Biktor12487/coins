4e4c7928b70d37214ca21db8d751dbb5415a6bab
Biktor12487 <biktor12487@gmail.com>
2017-03-28 21:04:59 +0300

    v 0.2 Edit coin/add/remove

---
+++ module/coin_add.php
@@ -0,0 +1,83 @@
+<?php 
+function list_year_coins($db){
+	$list_html= "";
+	$query = mysqli_query($db,"SELECT * FROM year");
+	while ($row = mysqli_fetch_array($query)) {
+			$list_html.='<option value="'.$row[0].'">'.$row[1].'</option>';
+		}
+		echo $list_html;
+}
+function list_country_coins($db){
+	$list_html= "";
+	$query = mysqli_query($db,"SELECT * FROM country");
+	while ($row = mysqli_fetch_array($query)) {
+		$list_html.='<option value="'.$row[0].'">'.$row[2].'</option>';
+	}
+	echo $list_html;
+}
+function add_coins($img,$title,$about,$year,$country,$db){
+	$imgLink = loadImg($img,'coins_img/');
+	mysqli_query($db,"INSERT INTO `coins` (`id`, `img`, `title`, `about`, `country`, `year`) VALUES (NULL, '$imgLink', '$title', '$about', '$country', '$year')");
+}
+function list_coins($db){
+	$list_html = "";
+	$query = mysqli_query($db,"SELECT * FROM coins");
+	while ($row = mysqli_fetch_array($query)) {
+		$year_id = $row[5];
+		$country_id = $row[4];
+		$year = mysqli_fetch_array(mysqli_query($db,"SELECT * FROM year WHERE id='$year_id'"))[1];
+		$country = mysqli_fetch_array(mysqli_query($db,"SELECT * FROM country WHERE id='$country_id'"))[2];
+		$list_html.= '<div class="rowCoin">
+			<img src="'.$row[1].'" alt="">
+			<h4>'.$row[2].'</h4>
+			<span>'.$year.'</span>
+			<span>'.$country.'</span>
+			<a href="?del_coin='.$row[0].'">Видалити</a>
+			<a href="?edit_coin='.$row[0].'">Редагувати</a>
+		</div>';
+	}
+	echo $list_html;
+}
+function del_coin($id,$db){
+	mysqli_query($db,"DELETE FROM `coins` WHERE id='$id'");
+}
+function edit_coin($id,$db){
+	$query = mysqli_query($db,"SELECT * FROM coins WHERE id='$id'");
+	if ($row = mysqli_fetch_array($query)) {
+		echo '	<h2 class="title">Редагувати</h2>
+				<div class="editCoins">
+ 				<form action="" method="post"  enctype="multipart/form-data">
+ 					<img src="'.$row[1].'" alt="">
+ 					<input type="file" name="img_coin" accept="image/jpg, image/JPG,image/JPEG, image/jpeg" >
+ 					<span>Назва</span>
+ 					<input type="text" name="title_coin" placeholder="Назва Монети" value="'.$row[2].'" required>
+ 					<span>Про монету</span>
+ 					<textarea name="about_coin" id="" cols="30" rows="10" placeholder="Про монету" required>'.$row[3].'</textarea>
+ 					<span>Рік</span>
+ 					<select name="year_coin" id="" required>';
+ 						list_year_coins($db);
+ 					echo '</select>
+ 					<span>Країна</span>
+ 					<select name="country_coin" id="" required>';
+ 					list_country_coins($db);
+ 					echo '</select>
+ 					<input type="submit" value="Редагувати" name="edit_coin">
+ 				</form>
+ 			</div>';
+	}
+	if (isset($_POST['edit_coin'])) {
+		$title = $_POST['title_coin'];
+ 		$about = $_POST['about_coin'];
+ 		$year = $_POST['year_coin'];
+ 		$country = $_POST['country_coin'];
+ 		$imgLink = loadImg('img_coin','coins_img/');
+ 		if ($imgLink == NULL) {
+ 			mysqli_query($db,"UPDATE `coins` SET `title`='$title',`about`='$about',`country`='$country',`year`='$year' WHERE id='$id'");
+ 		}
+ 		else
+ 		{
+ 			mysqli_query($db,"UPDATE `coins` SET `img`='$imgLink',`title`='$title',`about`='$about',`country`='$country',`year`='$year' WHERE id='$id'");
+ 		}
+	}
+}
+ ?>
--- page/admin/index.php
+++ page/admin/index.php
@@ -2,6 +2,7 @@
 	  include "../../module/header.php";
 	   include "../../module/add_country.php";
 	   include "../../module/list_country.php";
+	   include "../../module/coin_add.php";
 	  include "../../config.ini";
 	  $db = connect_db($hostName,$hostLogin,$hostPass,$dbName);
 
@@ -16,6 +17,19 @@
  		del_country($id,$db);
  		refresh($siteName.'page/admin/');
  	}
+ 	if (isset($_POST['add_coin'])) {
+ 		$title = $_POST['title_coin'];
+ 		$about = $_POST['about_coin'];
+ 		$year = $_POST['year_coin'];
+ 		$country = $_POST['country_coin'];
+ 		add_coins("img_coin",$title,$about,$year,$country,$db);
+ 		refresh($siteName.'page/admin/');
+ 	}
+ 	if (isset($_GET['del_coin'])) {
+ 		$id = sql_save($_GET['del_coin']);
+ 		del_coin($id,$db);
+ 		refresh($siteName.'page/admin/');
+ 	}
 ?>
  <?php get_header("Адмін панель",$siteName); ?>
  <section>
@@ -23,13 +37,49 @@
  		<div class="wrap-add-country">
  			<h2 class="title">Додати країну</h2>
  			<form action="" method="post"  enctype="multipart/form-data">
- 				<input type="file" name="img_country" accept="image/jpg, image/JPG,image/JPEG, image/jpeg" >
- 				<input type="text" name="name_country" placeholder="Назва країни">
+ 				<input type="file" name="img_country" accept="image/jpg, image/JPG,image/JPEG, image/jpeg" required>
+ 				<input type="text" name="name_country" placeholder="Назва країни" required>
  				<input type="submit" name='add_country' value="Додати">
  			</form>
  			<h2 class="title">Список країн</h2>
  			<div class="listCountry">
  				<?php list_Country($db); ?>
+ 			</div>
+ 		</div>
+ 	</div>
+ </section>
+ <section>
+ 	<div class="container">
+ 		<div class="wrap-coins">
+ 			<h2 class="title">Монети</h2>
+ 			<div class="addCoins">
+ 				<form action="" method="post"  enctype="multipart/form-data">
+ 					<input type="file" name="img_coin" accept="image/jpg, image/JPG,image/JPEG, image/jpeg"  required>
+ 					<input type="text" name="title_coin" placeholder="Назва Монети" required>
+ 					<textarea name="about_coin" id="" cols="30" rows="10" placeholder="Про монету" required></textarea>
+ 					<select name="year_coin" id="" required>
+ 						<?php list_year_coins($db); ?>
+ 					</select>
+ 					<select name="country_coin" id="" required>
+ 						<?php list_country_coins($db); ?>
+ 					</select>
+ 					<input type="submit" value="Додати монету" name="add_coin">
+ 				</form>
+ 			</div>
+ 			<?php if (isset($_GET['edit_coin'])) {
+ 				$id = $_GET['edit_coin'];
+ 				edit_coin($id,$db);
+ 			} ?>
+ 			<div class="topPanelListCoin">
+ 				<span>Зображення</span>
+ 				<span>Заголовок</span>
+ 				<span>Рік</span>
+ 				<span>Країна</span>
+ 				<span>Видалити</span>
+ 				<span>Редагувати</span>
+ 			</div>
+ 			<div class="listCoins">
+ 				<?php list_coins($db); ?>
  			</div>
  		</div>
  	</div>
